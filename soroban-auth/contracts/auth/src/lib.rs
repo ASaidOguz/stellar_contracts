b2ac7afeac66df06900938d5b89e94f49d4a447b
Ahmet Said Oğuz <98320812+ASaidOguz@users.noreply.github.com>
2025-10-08 19:52:22 +0000

    custom auth added -tests needed.

--- soroban-auth/contracts/auth/src/lib.rs
+++ soroban-auth/contracts/auth/src/lib.rs
@@ -1,20 +1,20 @@
 #![no_std]
-use soroban_sdk::{contract, contractimpl,contracterror,contracttype, vec, Env, String, Vec};
+use soroban_sdk::{contract, contracterror, contractimpl, contracttype, symbol_short, Address, Env, Symbol,String};
 
 // error 
 #[contracterror]
 #[derive(Copy, Clone, Debug, Eq, PartialEq, PartialOrd, Ord)]
 #[repr(u32)]
 pub enum Error {
-    CallerNotAdmin = 1,
-    ContractUninitialize =2,
+     CallerNotAdmin = 1,
+     ContractUninitialized =2,
+     ContractAlreadyInitialized=3,
 }
 
 #[contracttype]
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub struct InitState {
     pub admin: Address,
-    pub is_init: bool,
     pub init_block:u32,//// ledger sequence of init
 }
 
@@ -27,8 +27,42 @@ pub struct AuthContract;
 
 #[contractimpl]
 impl AuthContract {
-    pub fn hello(env: Env, to: String) -> Vec<String> {
-        vec![&env, String::from_str(&env, "Hello"), to]
+   pub fn init(env: Env) -> Result<(), Error> {
+    // If already initialized, return error
+    if Self::get_initstate(env.clone()).is_some() {
+        return Err(Error::ContractAlreadyInitialized);
+    }
+    
+    // set admin address;
+    let admin = Address::from_string(
+        &String::from_str(&env,"GBLEM3MIX2B6EUSLMCQ252INUCYNKN2WQDJWTOKN7EJPBY5EI4WLDYHB"));
+
+    let state = InitState {
+        admin,
+        init_block: env.ledger().sequence(),
+    };
+
+    env.storage().instance().set(&INITSTATE, &state);
+    env.storage().instance().set(&COUNTER, &0u32);
+
+    Ok(())
+}
+
+    // only admin can use increment;
+    pub fn increment(env: Env, value: u32) -> Result<u32, Error> {
+      let init_state = Self::get_initstate(env.clone()).ok_or(Error::ContractUninitialized)?;
+      init_state.admin.require_auth();
+      // u can safely use unwrap cause it will initalized with value;
+      let mut counter :u32 = env.storage().instance().get(&COUNTER).unwrap();
+      counter+=value;
+      
+      env.storage().instance().set(&COUNTER,&counter);
+      Ok(counter)
+    }
+
+    /// Return the init-state.
+    pub fn get_initstate(env: Env) -> Option<InitState> {
+        env.storage().instance().get(&INITSTATE)
     }
 }
 
