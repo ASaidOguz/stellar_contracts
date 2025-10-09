98df714cc9452ef5ec90e160d7ea40384c473257
Ahmet Said Oğuz <98320812+ASaidOguz@users.noreply.github.com>
2025-10-09 14:55:11 +0000

    auth perfeclty implemented and tested .

--- soroban-auth/contracts/auth/src/lib.rs
+++ soroban-auth/contracts/auth/src/lib.rs
@@ -1,5 +1,8 @@
 #![no_std]
-use soroban_sdk::{contract, contracterror, contractimpl, contracttype, symbol_short, Address, Env, Symbol,String};
+use soroban_sdk::{contract, contracterror, 
+                  contractimpl, contracttype, 
+                  symbol_short, Address, 
+                  Env, Symbol};
 
 // error 
 #[contracterror]
@@ -27,36 +30,31 @@ pub struct AuthContract;
 
 #[contractimpl]
 impl AuthContract {
-   pub fn init(env: Env) -> Result<(), Error> {
-    // If already initialized, return error
-    if Self::get_initstate(env.clone()).is_some() {
-        return Err(Error::ContractAlreadyInitialized);
-    }
-    
-    // set admin address;
-    let admin = Address::from_string(
-        &String::from_str(&env,"GBLEM3MIX2B6EUSLMCQ252INUCYNKN2WQDJWTOKN7EJPBY5EI4WLDYHB"));
-
+   pub fn __constructor(env: Env,admin_address:Address)  {
+  
     let state = InitState {
-        admin,
+        admin:admin_address,
         init_block: env.ledger().sequence(),
-    };
+        };
 
     env.storage().instance().set(&INITSTATE, &state);
     env.storage().instance().set(&COUNTER, &0u32);
-
-    Ok(())
-}
+  
+    }
 
     // only admin can use increment;
     pub fn increment(env: Env, value: u32) -> Result<u32, Error> {
       let init_state = Self::get_initstate(env.clone()).ok_or(Error::ContractUninitialized)?;
       init_state.admin.require_auth();
       // u can safely use unwrap cause it will initalized with value;
-      let mut counter :u32 = env.storage().instance().get(&COUNTER).unwrap();
+      let mut counter :u32 = env.storage()
+                                .instance()
+                                .get(&COUNTER).unwrap();
       counter+=value;
       
-      env.storage().instance().set(&COUNTER,&counter);
+      env.storage()
+         .instance()
+         .set(&COUNTER,&counter);
       Ok(counter)
     }
 
--- soroban-auth/contracts/auth/src/test.rs
+++ soroban-auth/contracts/auth/src/test.rs
@@ -1,2 +1,61 @@
 #![cfg(test)]
 
+use crate::{AuthContract, AuthContractClient};
+use soroban_sdk::{testutils::{Address as _, MockAuth, MockAuthInvoke}, Address, Env, IntoVal};
+
+
+#[test]
+fn test_admin_increment() {
+    let env = Env::default();
+    let admin = Address::generate(&env);                   
+    //Single-element tuple! as constructor arg
+    let contract_id = env.register(AuthContract, (&admin,));
+    let client = AuthContractClient::new(&env, &contract_id);
+    
+    // ✅ Test increment WITH authentication (admin required)
+    let count =client
+        .mock_auths(&[MockAuth {
+            address: &admin,
+            invoke: &MockAuthInvoke {
+                contract: &contract_id,
+                fn_name: "increment",
+                args: (5u32,).into_val(&env),
+                sub_invokes: &[],
+            },
+        }])
+        .increment(&5);
+        // assert returned value;
+        assert_eq!(&count,&5);        
+}
+
+#[test]
+#[should_panic]
+fn test_nonadmin_increment(){
+
+    let env = Env::default();
+    let admin = Address::generate(&env);
+    let non_admin = Address::generate(&env);    
+
+    //Single-element tuple! as constructor arg
+    let contract_id = env.register(AuthContract, (&admin,));
+    let client = AuthContractClient::new(&env, &contract_id);
+    
+    // ✅ Test increment WITH authentication (admin required)
+    let _ =client
+        .mock_auths(&[MockAuth {
+            address: &non_admin,
+            invoke: &MockAuthInvoke {
+                contract: &contract_id,
+                fn_name: "increment",
+                args: (5u32,).into_val(&env),
+                sub_invokes: &[],
+            },
+        }])
+        .increment(&5);
+        // assert returned value;
+        //assert_eq!(&count,&5); -> no need to assert its gonna fail.      
+}
+
+// Soroban SDK create two functions 
+//1- increment which returns solid u32 type
+//2- try_increment which returns result type -> Result<u32,Error> 
